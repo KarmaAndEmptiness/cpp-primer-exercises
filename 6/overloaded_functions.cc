48571a41d963f4bba2ad0028d73b12d0f46bd9ec
KarmaAndEmptiness <3329277943@qq.com>
2024-11-12 07:03:33 +0000

    2024.11.12 updateâ

--- 6/overloaded_functions.cc
+++ 6/overloaded_functions.cc
@@ -18,11 +18,15 @@ using std::endl;
 //     return 0;
 // }
 
+// main 函数不能重载
+//  int main(int a);
+//  int main(double b);
+
 int main(void)
 {
     // It is an error for two functions to differ only in terms of their return types. If the parameter lists of two functions match but the return types differ, then the second declaration is an error
     int func(void);
-    // bool func(void); // error.两个函数只有返回类型不一样
+    // bool func(void); // error.两个函数只有返回类型不一样 而形参的类型和个数都是一样的
     bool func(int); // 函数的返回类型和形参列表都不一样
 
     // Because there is no conversion from const, we can pass a const object (or a pointer to const) only to the version with a const parameter. 带有low-level const的参数传入的时候没有转换成没有const的类型的办法，因此它只能使用带有const的版本
