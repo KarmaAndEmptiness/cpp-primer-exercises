eba2375c5a40ca3dc4f7c642566c4b7365cd63d5
KarmaAndEmptiness <m202240473@xs.ustb.edu.cn>
2024-12-15 13:56:42 +0000

    2024.12.15 update

--- 13/copy_assign_and_destroy.cc
+++ 13/copy_assign_and_destroy.cc
@@ -1,26 +1,39 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <memory>
 using std::cout;
 using std::endl;
+using std::make_shared;
 using std::string;
 using std::vector;
+class Sales_data
+{
+private:
+    string bookNo;
+};
+
 class Foo
 {
 public:
     // A constructor is the copy constructor if its first parameter is a reference to the class type and any additional parameters have default values
-    Foo();                     // default constructor
-    Foo(const Foo &, int = 1); // copy constructor
+    Foo() = default; // default constructor
+    // Foo(const Foo &, int = 1)=default; // copy constructor,这里不能使用default
+    Foo(const Foo &) = default; // copy constructor
     // If that parameter were not a reference, then the call would never succeed—to call the copy constructor, we’d need to use the copy constructor to copy the argument, but to copy the argument, we’d need to call the copy constructor, and so on indefinitely.
     //  这样定义copy构造函数会导致无限递归
     //  Foo(const Foo,int=1); // copy constructor
 
     // To be consistent with assignment for the built-in types, assignment operators usually return a reference to their left-hand operand
-    Foo &operator=(const Foo &); // assignment operator
+    Foo &operator=(const Foo &) = default; // assignment operator
 
     // Because it takes no parameters, it cannot be overloaded.
     ~Foo(); // destructor
 };
+
+//  If we do not want the synthesized member to be an inline function, we can specify = default on the member’s definition
+Foo::~Foo() = default;
+
 struct Bar
 {
     /* data */
@@ -30,7 +43,42 @@ struct Bar
 struct Test
 {
     /* data */
-    int b;
+    const int b;
+};
+
+struct X
+{
+    X() { std::cout << "X()" << std::endl; }
+    X(const X &) { std::cout << "X(const X&)" << std::endl; }
+    X &operator=(const X &)
+    {
+        cout << "X& operator=(const X&)" << endl;
+        return *this;
+    }
+    ~X() { cout << "~X()" << endl; }
+};
+
+void func(const X x)
+{
+}
+
+void f(vector<int> vec)
+{
+}
+
+struct NoCopy
+{
+    NoCopy() = default;                         // use the synthesized default constructor
+    NoCopy(const NoCopy &) = delete;            // no copy
+    NoCopy &operator=(const NoCopy &) = delete; // no assignment
+    ~NoCopy() = default;                        // use the synthesized destructor
+    // other members
+};
+struct NoDtor
+{
+    NoDtor() = default; // use the synthesized default constructor
+    ~NoDtor() = delete; // we can't destroy objects of type NoDtor
+    int a;
 };
 
 int main()
@@ -45,7 +93,7 @@ int main()
     int arr[1] = {a};
     Test test = {a};
 
-    Bar Bar = {a}; // 这个不是copy initialize
+    Bar bar = {a}; // 这个不是copy initialize
     // const int &c[1]={a};
 
     string dots(10, '.');               // direct initialization
@@ -62,5 +110,50 @@ int main()
     f(vector<int>(10)); // ok: directly construct a temporary vector from an int
 
     cout << "a: " << a << endl;
+
+    // The destructor is not run when a reference or a pointer to an object goes out of scope.
+    { // new scope
+        // p and p2 point to dynamically allocated objects
+        Sales_data *p = new Sales_data;      // p is a built-in pointer
+        auto p2 = make_shared<Sales_data>(); // p2 is a shared_ptr
+        Sales_data item(*p);                 // copy constructor copies *p into item
+        vector<Sales_data> vec;              // local object
+        vec.push_back(*p2);                  // copies the object to which p2 points
+        delete p;                            // destructor called on the object pointed to by p
+    } // exit local scope; destructor called on item, p2, and vec
+      // destroying p2 decrements its use count; if the count goes to 0, the object is freed
+      // destroying vec destroys the elements in vec
+
+    cout << "--------------" << endl;
+    cout << "X x: " << endl;
+    X x;
+    cout << "--------------" << endl;
+    cout << "func(x): " << endl;
+    func(x);
+    cout << "--------------" << endl;
+    cout << "X *x_ptr: " << endl;
+    X *x_ptr = new X;
+
+    cout << "--------------" << endl;
+    cout << "delete x_ptr" << endl;
+    delete x_ptr;
+    cout << "--------------" << endl;
+    cout << "vec_X.push_back(x): " << endl;
+    vector<X> vec_X;
+    vec_X.push_back(x);
+    cout << "--------------" << endl;
+
+    // NoDtor nd;  // error: NoDtor destructor is deleted
+    NoDtor *p = new NoDtor(); // ok: but we can't delete p
+    // delete p; // error: NoDtor destructor is deleted
+
+    //    has a reference member that does not have an in-class initializer
+    // Bar bb;//delete constructor
+
+    // The synthesized default constructor is defined as deleted if the class has a member with a deleted or inaccessible destructor;
+    // NoDtor noDtor;//error
+
+    // or has a const member whose type does not explicitly define a default constructor and that member does not have an in-class initializer.
+    // Test t;//error
     return 0;
 }
