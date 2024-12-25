1f587757e523e5bac6de75a6b19028a8915c4f1d
KarmaAndEmptiness <m202240473@xs.ustb.edu.cn>
2024-12-25 13:59:27 +0000

    2024.12.25 update

--- 10/customizing_operations.cc
+++ 10/customizing_operations.cc
@@ -5,6 +5,7 @@
 #include <functional>
 using std::cout;
 using std::endl;
+using std::ostream;
 using std::string;
 using std::vector;
 
@@ -18,6 +19,27 @@ bool isLongerThan(const std::string &str, size_t size)
     return str.size() > size;
 }
 
+void fcn2()
+{
+    size_t v1 = 42; // local variable
+    // the object f2 contains a reference to v1
+    auto f2 = [&v1]
+    { return v1; };
+    v1 = 0;
+    auto j = f2(); // j is 0; f2 refers to v1; it doesn't store it
+}
+
+// Reference captures are sometimes necessary. For example, we might want our biggies function to take a reference to an ostream on which to write and a character to use as the separator
+void biggies(vector<string> &words,
+             vector<string>::size_type sz,
+             ostream &os = cout, char c = ' ')
+{
+    // code to reorder words as before
+    // statement to print count revised to print to os
+    for_each(words.begin(), words.end(),
+             [&os, c](const string &s)
+             { os << s << c; });
+}
 int main()
 {
     vector<string> words = {"world!", "hello", "abcdef"};
@@ -79,6 +101,19 @@ int main()
         auto f = [](int a = 1)
         { return a; };
         f();
+    }
+
+    {
+        int sz = 6;
+        // get an iterator to the first element whose size() is >= sz
+        auto wc = find_if(words.begin(), words.end(),
+                          [sz](const string &a)
+                          { return a.size() >= sz; });
+        // print words of the given size or longer, each one followed by a space
+        for_each(wc, words.end(),
+                 [](const string &s)
+                 { cout << s << " "; });
+        cout << endl;
     }
     return 0;
 }
